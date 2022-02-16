c40309ef56ec079a7dbce1b8df0215f63fa5391e
Greg Doud <gdoud@gmail.com>
2022-02-16 16:27:12 -0500

    Add project files.

---
+++ SalesWebApi/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebApi.Models {
+
+    public class AppDbContext : DbContext {
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+        }
+    }
+}
---
+++ SalesWebApi/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesWebApi.Models {
+    
+    public class Customer {
+
+        public int Id { get; set; }
+        [Required, StringLength(30)]
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        [Required, StringLength(2)]
+        public string StateCode { get; set; }
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Sales { get; set; }
+
+        public Customer() { }
+    }
+}
